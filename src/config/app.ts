cf86c1d7ab91a72b620cbec01aec5f852b9d20b9
Łukasz Wójcik <lwojcik@lukem.net>
2021-06-12 00:16:48 +0200

    cache implementation

--- src/config/app.ts
+++ src/config/app.ts
@@ -2,4 +2,4 @@ import { boolean } from '../utils';
 
 export const enableLogging = boolean(process.env.FN_LOGGING_ENABLE);
 export const enableVerboseLogging = boolean(process.env.FN_LOGGING_VERBOSE_ENABLE);
-export const cacheDir = process.env.FN_CACHE_DIRECTORY;
+export const cacheDirectory = process.env.FN_CACHE_DIRECTORY!;
--- src/lib/cache.ts
+++ src/lib/cache.ts
@@ -1,16 +1,46 @@
+import fs from 'fs';
+import path from 'path';
+import { app } from '../config';
+import { logger } from './logger';
+
+const { cacheDirectory } = app;
+
+const createDirectoryMaybe = (directory: string) => {
+  if (!fs.existsSync(directory)) {
+    fs.mkdirSync(directory);
+  }
+};
+
+interface SaveToFileArgs {
+  directory: string;
+  file: string;
+  content: string;
+}
+
+const saveToFile = ({
+  directory,
+  file,
+  content,
+}: SaveToFileArgs) => {
+  createDirectoryMaybe(directory);
+  const filePath = path.join(directory, file);
+  fs.writeFileSync(filePath, content);
+};
+
+const getFileContents = (filePath: string) =>
+  fs.readFileSync(filePath);
+
 export const cache = {
   get: (name: string) => {
     console.log(`getting cached value for ${name}...`); // eslint-disable-line
-    return 'bar';
+    return getFileContents(path.join(cacheDirectory, name));
   },
-  set: (name: string, value: string | object) => {
-    console.log(name); // eslint-disable-line
-    console.log(value); // eslint-disable-line
-    console.log(`setting value for ${name} to ${value.toString()}...`); // eslint-disable-line
-    return true;
-  },
-  delete: (name: string) => {
-    console.log(`deleting value for ${name}...`); // eslint-disable-line
-    return true;
+  set: (name: string, value: object) => {
+    logger.verbose(`Saving new article to ${path.join(cacheDirectory, name)}...`); // eslint-disable-line
+    saveToFile({
+      directory: cacheDirectory,
+      file: name,
+      content: JSON.stringify(value),
+    });
   },
 };
