47cd509fa2e0b56f3264a75bed0c684c179325ed
Łukasz Wójcik <lwojcik@lukem.net>
2021-06-13 15:55:48 +0200

    add more test coverage

--- tests/models/Feed.test.ts
+++ tests/models/Feed.test.ts
@@ -1,94 +1,63 @@
 import { Feed } from '../../src/models/Feed';
 
-jest.mock('rss-parser', () => jest.fn(() => ({
-  postStatus: () => ({
-    data: {
-      uri: 'sample_uri_from_inline_megalodon_mock',
+jest.mock('rss-parser', () => jest.fn().mockImplementation(() => ({
+  parseURL: () => Promise.resolve({
+    content: {
+      lastBuildDate: 'mocked_last_build_date',
     },
+    lastBuildDate: 'mocked_last_build_date',
+    items: [
+      {
+        pubDate: 'mocked_last_item_pub_date',
+        title: 'mocked_last_item_title',
+        link: 'mocked_last_item_link',
+      },
+    ],
   }),
 })));
 
 describe('Feed model', () => {
-  const sampleFeed = new Feed('sample_feed_url');
+  const feedInstance = new Feed('sample_feed_url');
+
+  const feedMethods = [
+    'fetch',
+    'getFeedUpdateDate',
+    'getLatestPostDate',
+    'getLatestPostTitle',
+    'getLatestPostUrl',
+  ];
 
   it('should be defined', () => {
     expect(Feed).toBeDefined();
   });
 
-  it('should define fetch method', () => {
-    expect(sampleFeed.fetch).toBeDefined();
+  it('should implement all methods', () => {
+    feedMethods.forEach(method => {
+      expect((feedInstance as any)[method]).toBeDefined();
+    });
   });
 
-  it('should define getFeedUpdateDate method', () => {
-    expect(sampleFeed.getFeedUpdateDate).toBeDefined();
+  it('should return feed update date', async () => {
+    expect.assertions(1);
+    const feed = await feedInstance.fetch();
+    expect(feed.getFeedUpdateDate()).toMatchSnapshot();
   });
 
-  it('should define getLatestPostDate method', () => {
-    expect(sampleFeed.getLatestPostDate).toBeDefined();
+  it('should return latest post date', async () => {
+    expect.assertions(1);
+    const feed = await feedInstance.fetch();
+    expect(feed.getLatestPostDate()).toMatchSnapshot();
   });
 
-  it('should define getLatestPostTitle method', () => {
-    expect(sampleFeed.getLatestPostTitle).toBeDefined();
+  it('should return latest post title', async () => {
+    expect.assertions(1);
+    const feed = await feedInstance.fetch();
+    expect(feed.getLatestPostTitle()).toMatchSnapshot();
   });
 
-  it('should define getLatestPostUrl method', () => {
-    expect(sampleFeed.getLatestPostUrl).toBeDefined();
+  it('should return latest post URL', async () => {
+    expect.assertions(1);
+    const feed = await feedInstance.fetch();
+    expect(feed.getLatestPostUrl()).toMatchSnapshot();
   });
 });
-
-// import Parser from 'rss-parser';
-
-// interface FeedItem {
-//   title: string;
-//   pubDate: string;
-//   link: string;
-//   id: string;
-// }
-
-// interface FeedContent {
-//   lastBuildDate: string;
-//   items: Array<FeedItem>;
-// }
-
-// export class Feed {
-//   url: string;
-
-//   private content: FeedContent | undefined;
-
-//   constructor(url: string) {
-//     this.url = url;
-//   }
-
-//   async fetch() {
-//     this.content = await new Parser({
-//       customFields: {
-//         feed: ['lastBuildDate'],
-//       },
-//     }).parseURL(this.url) as FeedContent;
-//     return this as Feed;
-//   }
-
-//   private getFeed() {
-//     return this.content!;
-//   }
-
-//   private getLatestItem() {
-//     return this.getFeed().items[0] as FeedItem;
-//   }
-
-//   getFeedUpdateDate() {
-//     return this.getFeed().lastBuildDate;
-//   }
-
-//   getLatestPostDate() {
-//     return this.getLatestItem().pubDate;
-//   }
-
-//   getLatestPostTitle() {
-//     return this.getLatestItem().title;
-//   }
-
-//   getLatestPostUrl() {
-//     return this.getLatestItem().link;
-//   }
-// }
