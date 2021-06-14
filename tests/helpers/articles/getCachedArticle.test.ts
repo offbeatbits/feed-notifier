a6a8016a28c9ca5799f8a14960b199adad4d9d08
Łukasz Wójcik <lwojcik@lukem.net>
2021-06-14 21:13:16 +0200

    more test coverage

---
+++ tests/helpers/articles/getCachedArticle.test.ts
@@ -0,0 +1,30 @@
+import { Cache } from '../../../src/lib/cache';
+import { getCachedArticle } from '../../../src/helpers/articles/getCachedArticle';
+
+const sampleArticle = {
+  feedUpdateDate: '2021-06-04T11:20:42Z',
+  date: '2021-06-04T11:20:42Z',
+  title: 'sample_title_1',
+  url: 'sample_url_1',
+};
+
+jest.mock('../../../src/lib/cache', () => ({
+  Cache: {
+    get: jest.fn().mockImplementation(() => JSON.stringify(sampleArticle)),
+  },
+}));
+
+describe('getCachedArticle', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should get cached article', () => {
+    expect(getCachedArticle()).toMatchSnapshot();
+  });
+
+  it('should return null if Cache contains no article', () => {
+    (Cache as any).get = () => null;
+    expect((getCachedArticle())).toBeNull();
+  });
+});
---
+++ tests/helpers/articles/updateArticleCache.test.ts
@@ -0,0 +1,33 @@
+import { Cache } from '../../../src/lib/cache';
+import { updateArticleCache } from '../../../src/helpers/articles/updateArticleCache';
+
+const sampleArticle = {
+  feedUpdateDate: '2021-06-04T11:20:42Z',
+  date: '2021-06-04T11:20:42Z',
+  title: 'sample_title_1',
+  url: 'sample_url_1',
+};
+
+jest.mock('../../../src/lib/cache', () => ({
+  Cache: {
+    // eslint-disable-next-line no-empty-pattern
+    set: jest.fn().mockImplementation(({}, value: any) =>
+      JSON.stringify(value)),
+  },
+}));
+
+describe('updateArticleCache', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should update article cache', () => {
+    updateArticleCache(sampleArticle);
+    expect(Cache.set).toHaveBeenCalledTimes(1);
+  });
+
+  it('should not update article cache if null is passed', () => {
+    updateArticleCache(null);
+    expect(Cache.set).toHaveBeenCalledTimes(0);
+  });
+});
