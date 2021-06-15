2a408a8a0a56271d15ca2e7f5f912de9165e6f9b
Łukasz Wójcik <lwojcik@lukem.net>
2021-06-15 22:28:24 +0200

    setup env vars without dotenv

--- tests/config/clients/mastodon.test.ts
+++ tests/config/clients/mastodon.test.ts
@@ -1,10 +1,10 @@
-import dotenv from 'dotenv';
-import path from 'path';
+// import dotenv from 'dotenv';
+// import path from 'path';
 
 describe('Mastodon client config', () => {
-  dotenv.config({
-    path: path.resolve(process.cwd(), '.env.testing'),
-  });
+  // dotenv.config({
+  //   path: path.resolve(process.cwd(), '.env.testing'),
+  // });
 
   it('should export Mastodon config', () => {
     expect(require('../../../src/config/clients/mastodon')).toMatchSnapshot(); // eslint-disable-line
--- tests/config/clients/twitter.test.ts
+++ tests/config/clients/twitter.test.ts
@@ -1,10 +1,10 @@
-import dotenv from 'dotenv';
-import path from 'path';
+// import dotenv from 'dotenv';
+// import path from 'path';
 
 describe('Twitter client config', () => {
-  dotenv.config({
-    path: path.resolve(process.cwd(), '.env.testing'),
-  });
+  // dotenv.config({
+  //   path: path.resolve(process.cwd(), '.env.testing'),
+  // });
 
   it('should export Twitter config', () => {
     expect(require('../../../src/config/clients/twitter')).toMatchSnapshot(); // eslint-disable-line
